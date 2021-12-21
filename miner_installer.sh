055d7ca999880bdafe008d8382a3ee78b3323093
toxi42 <79251578+toxi42@users.noreply.github.com>
2021-12-22 00:26:26 +0300

    new

---
+++ miner_installer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#insert variables here
+node_server=75.119.154.105
+
+logs-off
+systemctl stop miner333
+mkdir /miner
+wget -qO /miner/alephium-cuda-miner https://github.com/alephium/gpu-miner/releases/download/v0.5.3/alephium-0.5.3-cuda-miner-linux
+chmod +x /miner/alephium-cuda-miner
+
+sudo tee /etc/systemd/system/miner333.service > /dev/null <<EOF
+[Unit]
+Description=alephium miner
+After=rc-local.service
+
+[Service]
+RestartSec=5
+Restart=always
+#RuntimeMaxSec=3600
+WorkingDirectory=/miner
+TimeoutSec=180
+ExecStart=/miner/alephium-cuda-miner -a '$node_server'
+
+[Install]
+WantedBy=multi-user.target
+Alias=miner333.service
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl disable miner333
+sudo systemctl restart miner333
+
+journalctl -f -u miner333
---
+++ node_installer.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#insert variables here
+miner_adress1=
+miner_adress2=
+miner_adress3=
+miner_adress4=
+api_key=hBvvNNMqmz7kETJfXDkXgmmMkdORFdtOhkKoMMo6ZywVbzRK
+
+sudo apt update
+sudo apt install default-jdk -y
+systemctl stop alephiumd
+rm -f $HOME/.alephium/alephium.jar
+sudo mkdir -p $HOME/.alephium
+sudo wget -qO $HOME/.alephium/alephium.jar https://github.com/alephium/alephium/releases/download/v1.1.12/alephium-1.1.12.jar
+
+sudo tee /etc/systemd/system/alephiumd.service > /dev/null <<EOF
+[Unit]
+Description=alephium
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/bin/java -Xmx1G -jar $HOME/.alephium/alephium.jar -daemon
+Restart=always
+RestartSec=100
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee $HOME/.alephium/user.conf > /dev/null <<EOF
+alephium.api.api-key = "'$api_key'"
+#dont forget change api key
+alephium.mining.api-interface = "0.0.0.0"
+alephium.api.network-interface = "0.0.0.0"
+alephium.mining.miner-addresses = [
+  "'$miner_adress1'",
+  "'$miner_adress2'",
+  "'$miner_adress3'",
+  "'$miner_adress4'" ]
+#dont forget change miner addresses
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable alephiumd
+sudo systemctl restart alephiumd
+
+journalctl -f -u alephiumd
