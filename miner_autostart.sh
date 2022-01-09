9132f6b806dc607cdc831086ca272b04594be20b
toxi42 <79251578+toxi42@users.noreply.github.com>
2022-01-09 03:13:30 +0300

    Delete miner_autostart.sh

--- miner_autostart.sh
+++
@@ -1,9 +0,0 @@
-sudo tee /home/user/xinit.user.sh > /dev/null <<EOF
-#!/usr/bin/env bash
-
-# This script is run in terminal after X server start
-
-# uncomment the following line if you want to see miner log after start
-#tail -f /run/hive/miner.1
-systemctl start miner333
-EOF
