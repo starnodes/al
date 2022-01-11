64c68e770893ce1ae2b27b15e2527ae4612948e5
toxi42 <79251578+toxi42@users.noreply.github.com>
2022-01-11 14:31:06 +0300

    Update transfer_alph.sh

--- transfer_alph.sh
+++ transfer_alph.sh
@@ -6,6 +6,10 @@ wallet_name=
 wallet_pass=
 server=127.0.0.1:12973
 address_for_sending=
+miner_adress1=
+miner_adress2=
+miner_adress3=
+miner_adress4=
 
 #unlock wallet
 curl -X 'POST' \
@@ -17,13 +21,93 @@ curl -X 'POST' \
   "password": "'$wallet_pass'"
 }'
 
+#change active adress to 1
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/change-active-address' \
+  -H 'accept: */*' \
+  -H 'X-API-KEY: '$api_key'' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "address": "'$miner_adress1'"
+}'
 
 #transfer all ulocked coins
 curl -X 'POST' \
-  'http://'$server'/wallets/'$wallet_name'/sweep-all-addresses' \
+  'http://'$server'/wallets/'$wallet_name'/sweep-all' \
   -H 'accept: application/json' \
   -H 'X-API-KEY: '$api_key'' \
   -H 'Content-Type: application/json' \
   -d '{
   "toAddress": "'$address_for_sending'"
+}'
+
+#change active adress to 2
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/change-active-address' \
+  -H 'accept: */*' \
+  -H 'X-API-KEY: '$api_key'' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "address": "'$miner_adress2'"
+}'
+
+#transfer all ulocked coins
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/sweep-all' \
+  -H 'accept: application/json' \
+  -H 'X-API-KEY: '$api_key'' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "toAddress": "'$address_for_sending'"
+<<<<<<< HEAD
     }'
+=======
+}'
+
+#change active adress to 3
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/change-active-address' \
+  -H 'accept: */*' \
+  -H 'X-API-KEY: '$api_key'' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "address": "'$miner_adress3'"
+}'
+
+#transfer all ulocked coins
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/sweep-all' \
+  -H 'accept: application/json' \
+  -H 'X-API-KEY: '$api_key'' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "toAddress": "'$address_for_sending'"
+}'
+
+#change active adress to 4
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/change-active-address' \
+  -H 'accept: */*' \
+  -H 'X-API-KEY: '$api_key'' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "address": "'$miner_adress4'"
+}'
+
+#transfer all ulocked coins
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/sweep-all' \
+  -H 'accept: application/json' \
+  -H 'X-API-KEY: '$api_key'' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "toAddress": "'$address_for_sending'"
+}'
+
+#lock wallet
+curl -X 'POST' \
+  'http://'$server'/wallets/'$wallet_name'/lock' \
+  -H 'accept: */*' \
+  -H 'X-API-KEY: '$api_key'' \
+  -d ''
+>>>>>>> parent of 40c448d (Update transfer_alph.sh)
